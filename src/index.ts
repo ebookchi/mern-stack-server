7b71fd9351a6544f753605ab7e330922130781bc
Babak Jafari <Babak.Jafari4@gmail.com>
2025-05-16 06:56:59 +0100

    feat(api): add express server entry point
    add initial TypeScript express server in src/index.ts and update dependencies for type support

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// import { createServer } from "http";
+
+const app = express();
+// const server = createServer(app);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
